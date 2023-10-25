05f46980df98f1ce5e8729cb24879b909bbda7e5
B3rcik <olaf.chromik1@wp.pl>
2023-10-25 21:24:36 +0200

    Hello

---
+++ pierwszy rok/nauka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace nauka {
+    class Podstawy
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ pierwszy rok/zadanie 3/Program3.cs
@@ -0,0 +1,26 @@
+﻿namespace zadanie3
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("podaj pierwszą liczbe");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("podaj drugą liczbe");
+            b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("podaj trzecią liczbe");
+            c=Convert.ToInt32(Console.ReadLine());
+            if (a>b)
+            {
+                if (c>a) { Console.WriteLine(c); }
+                else { Console.WriteLine(a); }
+
+
+            }else { if (b > c) { Console.WriteLine(b); }
+                else { Console.WriteLine(c); }
+            }
+
+        }
+    }
+}
